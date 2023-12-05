4e87f06bb074f2ed498d74f871578cde8a69b41c
Ash <ashwinirao.karai@gmail.com>
2023-12-05 01:08:28 -0700

    employ builder pattern to create objects

---
+++ src/main/java/com/serenitydojo/builderpattern/Dog.java
@@ -0,0 +1,84 @@
+package com.serenitydojo.builderpattern;
+
+import java.time.LocalDateTime;
+
+public class Dog {
+    private String name, breed, color, optionalFavoriteFood, optionalFavoriteToy;
+    private LocalDateTime dateOfBirth;
+    public interface WithBreed{
+        public WithColor ofBreed(String breed);
+    }
+    public interface WithColor{
+        public  DogBuilder ofColor(String color);
+    }
+
+    private Dog(String name, String breed, String color, String optionalFavoriteFood, String optionalFavoriteToy, LocalDateTime dateOfBirth){
+        this.name = name;
+        this.breed = breed;
+        this.color = color;
+        this.optionalFavoriteFood = optionalFavoriteFood;
+        this.optionalFavoriteToy = optionalFavoriteToy;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public static WithBreed called(String name) {
+        return new DogBuilder(name);
+    }
+
+    public static class DogBuilder implements WithColor, WithBreed{
+        private String name, breed, color, optionalFavoriteFood, optionalFavoriteToy;
+        private LocalDateTime dateOfBirth;
+        public DogBuilder(String name){
+             this.name = name;
+        }
+
+        public WithColor ofBreed(String breed) {
+            this.breed = breed;
+            return this;
+        }
+
+        public  DogBuilder ofColor(String color){
+            this.color = color;
+            return this;
+        }
+
+        public DogBuilder likesToEat(String optionalFavoriteFood){
+            this.optionalFavoriteFood = optionalFavoriteFood;
+            return this;
+        }
+
+        public DogBuilder enjoysPlayingWith(String optionalFavoriteToy){
+            this.optionalFavoriteToy = optionalFavoriteToy;
+            return this;
+        }
+
+        public Dog bornOn(LocalDateTime dateOfBirth){
+            this.dateOfBirth = dateOfBirth;
+            return new Dog(name, breed, color, optionalFavoriteFood, optionalFavoriteToy, dateOfBirth);
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBreed() {
+        return this.breed;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public String getOptionalFavoriteFood(){
+        return optionalFavoriteFood;
+    }
+
+    public String getOptionalFavoriteToy(){
+        return optionalFavoriteToy;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+}
---
+++ src/test/java/com/serenitydojo/builderpattern/WhenWeCreateANewDog.java
@@ -0,0 +1,76 @@
+package com.serenitydojo.builderpattern;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+public class WhenWeCreateANewDog {
+    private static final LocalDateTime FOURTH_OF_JULY = LocalDateTime.of(2023, Month.JULY, 4,0, 0);
+
+ /*   @Test
+    public void can_create_a_dog(){
+        *//*instantiate a dog object with 3 params - name, breed, and dateOfBirth
+        * assert that the qualities set on the dog are the same as those returned by the getter
+        * *//*
+        Dog cloud =  new Dog("cloud", "poodle", FOURTH_OF_JULY);
+
+        Assertions.assertThat(cloud.getName()).isEqualTo("cloud");
+        Assertions.assertThat(cloud.getBreed()).isEqualTo("poodle");
+        Assertions.assertThat(cloud.getDateOfBirth()).isEqualTo(FOURTH_OF_JULY);
+    }*/
+
+    @Test
+    public void can_create_a_dog_with_required_fields(){
+        //Design tips:
+        //write the structure for builder in calling code. Construct it like simple talking language.
+        //write the first method (static method in Dog) to set a field and pass and return the DogBuilder instance
+        //write the other builder methods
+        //finish with the last method and return a Dog instance
+
+            Dog fido = Dog.called("Fido")   //implicitly required field/ method - needed to trigger DogBreeder object
+                .ofBreed("Labrador")    //field/method made explicitly required thro interface type object
+                .ofColor("black")   //field/method made explicitly required thro interface type object
+                .bornOn(FOURTH_OF_JULY);    //implicitly required field - needed to trigger Dog object
+
+        Assertions.assertThat(fido.getName()).isEqualTo("Fido");
+        Assertions.assertThat(fido.getBreed()).isEqualTo("Labrador");
+        Assertions.assertThat(fido.getColor()).isEqualTo("black");
+        Assertions.assertThat(fido.getDateOfBirth()).isEqualTo(FOURTH_OF_JULY);
+    }
+
+    @Test
+    public void can_set_optional_fields(){
+        Dog cloud = Dog.called("cloud")
+                .ofBreed("poodle")
+                .ofColor("gray")
+                .likesToEat("biscuits")        //set optional field
+                .enjoysPlayingWith("ball")        //set optional field
+                .bornOn(FOURTH_OF_JULY);
+
+        Assertions.assertThat(cloud.getName()).isEqualTo("cloud");
+        Assertions.assertThat(cloud.getBreed()).isEqualTo("poodle");
+        Assertions.assertThat(cloud.getColor()).isEqualTo("gray");
+        Assertions.assertThat(cloud.getOptionalFavoriteFood()).isNotNull();
+        Assertions.assertThat(cloud.getOptionalFavoriteToy()).isNotNull();
+        Assertions.assertThat(cloud.getDateOfBirth()).isEqualTo(FOURTH_OF_JULY);
+    }
+
+    @Test
+    public void can_skip_optional_fields(){
+        Dog cloud = Dog.called("cloud")
+                .ofBreed("poodle")
+                .ofColor("gray")
+//                .likesToEat("biscuits")        //skip optional field
+//                .enjoysPlayingWith("ball")        //skip optional field
+                .bornOn(FOURTH_OF_JULY);
+
+        Assertions.assertThat(cloud.getName()).isEqualTo("cloud");
+        Assertions.assertThat(cloud.getBreed()).isEqualTo("poodle");
+        Assertions.assertThat(cloud.getColor()).isEqualTo("gray");
+        Assertions.assertThat(cloud.getOptionalFavoriteFood()).isNull();
+        Assertions.assertThat(cloud.getOptionalFavoriteToy()).isNull();
+        Assertions.assertThat(cloud.getDateOfBirth()).isEqualTo(FOURTH_OF_JULY);
+    }
+}
